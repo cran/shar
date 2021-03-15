d9cac427a4835a045ffb8a77b8412414a78474ae
Maximillian H.K. Hesselbarth <mhk.hesselbarth@gmail.com>
2021-03-15 12:30:02 +0000

    version 1.2

--- inst/doc/get_started.R
+++ inst/doc/get_started.R
@@ -1,4 +1,4 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -7,33 +7,33 @@ knitr::opts_chunk$set(
 ## ----load_libraries_hidden, eval = TRUE, echo = FALSE, message = FALSE, results = "hide"----
 library(dplyr)
 library(shar)
-library(spatstat)
+library(spatstat.core)
 library(raster)
 
-## ----classify_habitats---------------------------------------------------
+## ----classify_habitats--------------------------------------------------------
 landscape_discrete <- classify_habitats(raster = landscape, classes = 5)
 
-## ----randomize_raster, eval = FALSE--------------------------------------
+## ----randomize_raster, eval = FALSE-------------------------------------------
 #  torus_trans <- translate_raster(raster = landscape_discrete)
 #  
 #  random_walk <- randomize_raster(raster = landscape_discrete, n_random = 39)
 
-## ----randomize_pp, eval = FALSE------------------------------------------
+## ----randomize_pp, eval = FALSE-----------------------------------------------
 #  gamma_test <- fit_point_process(pattern = species_a, n_random = 39, process = "cluster")
 #  
 #  reconstruction <- reconstruct_pattern_homo(pattern = species_a, n_random = 39) # takes some time
 
-## ----results-------------------------------------------------------------
+## ----results------------------------------------------------------------------
 results_habitat_association(pattern = species_a, raster = random_walk)
 
 results_habitat_association(pattern = reconstruction, raster = landscape_discrete)
 
-## ----plotting, eval = FALSE----------------------------------------------
+## ----plotting, eval = FALSE---------------------------------------------------
 #  plot_randomized_raster(random_walk)
 #  
 #  plot_randomized_pattern(reconstruction)
 
-## ----energy, message = FALSE---------------------------------------------
+## ----energy, message = FALSE--------------------------------------------------
 calculate_energy(pattern = gamma_test, return_mean = TRUE)
 
 calculate_energy(pattern = reconstruction, return_mean = TRUE)
