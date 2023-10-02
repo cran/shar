f2a24b89241b9af975f94527b9db87d85c969a49
Maximilian H.K. Hesselbarth <mhk.hesselbarth@gmail.com>
2023-10-02 09:50:02 +0000

    version 2.1

--- inst/doc/get_started.R
+++ inst/doc/get_started.R
@@ -19,6 +19,10 @@ landscape_discrete <- classify_habitats(raster = terra::rast(landscape), n = 5, 
 #  
 #  random_walk <- randomize_raster(raster = landscape_discrete, n_random = 39)
 
+## ----randomize_raster_import, echo = FALSE, eval = TRUE-----------------------
+torus_trans <- unpack_randomized(raster = shar:::data_internal$torus_trans)
+random_walk <- unpack_randomized(raster = shar:::data_internal$random_walk)
+
 ## ----randomize_pp, eval = FALSE-----------------------------------------------
 #  gamma_test <- fit_point_process(pattern = species_b, process = "cluster", n_random = 39)
 #  
@@ -26,21 +30,20 @@ landscape_discrete <- classify_habitats(raster = terra::rast(landscape), n = 5, 
 #  reconstruction <- reconstruct_pattern(pattern = species_b, n_random = 39,
 #                                        e_threshold = 0.05, method = "cluster")
 
-## ----result_import, echo = FALSE, eval = TRUE---------------------------------
-random_walk <- unpack_randomized(raster = random_walk)
+## ----randomize_import, echo = FALSE, eval = TRUE------------------------------
+gamma_test <- shar:::data_internal$gamma_test
+reconstruction <- shar:::data_internal$reconstruction
 
 ## ----results------------------------------------------------------------------
 results_habitat_association(pattern = species_a, raster = random_walk)
 
 results_habitat_association(pattern = reconstruction, raster = landscape_discrete)
 
-## ----plotting, fig.align = "center", out.height = "100%", out.width = "100%", message = FALSE----
-plot(random_walk)
+## ----plot_recon, eval = FALSE-------------------------------------------------
+#  plot(reconstruction, ask = FALSE)
 
-plot(reconstruction, ask = FALSE)
+## ----plot_tors, fig.align = "center", out.height = "65%", out.width = "65%", message = FALSE----
+col_palette <- c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF")
 
-## ----energy, message = FALSE--------------------------------------------------
-calculate_energy(pattern = gamma_test, return_mean = TRUE)
-
-calculate_energy(pattern = reconstruction, return_mean = TRUE)
+plot(torus_trans, col = col_palette)
 
