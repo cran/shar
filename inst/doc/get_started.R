f543515d1c9088d50bb84dabcec157a45fb55a55
Maximilian H.K. Hesselbarth <mhk.hesselbarth@gmail.com>
2025-01-10 13:10:02 +0000

    version 2.3.1

--- inst/doc/get_started.R
+++ inst/doc/get_started.R
@@ -15,20 +15,20 @@ set.seed(42)
 landscape_discrete <- classify_habitats(raster = terra::rast(landscape), n = 5, style = "fisher")
 
 ## ----randomize_raster, eval = FALSE-------------------------------------------
-#  torus_trans <- translate_raster(raster = landscape_discrete)
-#  
-#  random_walk <- randomize_raster(raster = landscape_discrete, n_random = 39)
+# torus_trans <- translate_raster(raster = landscape_discrete)
+# 
+# random_walk <- randomize_raster(raster = landscape_discrete, n_random = 39)
 
 ## ----randomize_raster_import, echo = FALSE, eval = TRUE-----------------------
 torus_trans <- unpack_randomized(raster = shar:::data_internal$torus_trans)
 random_walk <- unpack_randomized(raster = shar:::data_internal$random_walk)
 
 ## ----randomize_pp, eval = FALSE-----------------------------------------------
-#  gamma_test <- fit_point_process(pattern = species_b, process = "cluster", n_random = 39)
-#  
-#  # (this can takes some time)
-#  reconstruction <- reconstruct_pattern(pattern = species_b, n_random = 39,
-#                                        e_threshold = 0.05, method = "cluster")
+# gamma_test <- fit_point_process(pattern = species_b, process = "cluster", n_random = 39)
+# 
+# # (this can takes some time)
+# reconstruction <- reconstruct_pattern(pattern = species_b, n_random = 39,
+#                                       e_threshold = 0.05, method = "cluster")
 
 ## ----randomize_import, echo = FALSE, eval = TRUE------------------------------
 gamma_test <- shar:::data_internal$gamma_test
@@ -40,7 +40,7 @@ results_habitat_association(pattern = species_a, raster = random_walk)
 results_habitat_association(pattern = reconstruction, raster = landscape_discrete)
 
 ## ----plot_recon, eval = FALSE-------------------------------------------------
-#  plot(reconstruction, ask = FALSE)
+# plot(reconstruction, ask = FALSE)
 
 ## ----plot_tors, fig.align = "center", out.height = "65%", out.width = "65%", message = FALSE----
 col_palette <- c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF")
